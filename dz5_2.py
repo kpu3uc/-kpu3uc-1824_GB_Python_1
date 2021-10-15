333f77e5b27e41564c8b3552ed9a92b674133765
s4astie <serg_1982@mail.ru>
2021-10-15 17:12:18 +0300

    dz 5 2

---
+++ dz5_2.py
@@ -0,0 +1,15 @@
+def odd_numbers(max_odd):
+    return(_ for _ in range(1, max_odd + 1, 2))
+
+
+# print(list(odd_numbers(16)))
+test = odd_numbers(10)
+print(test)
+print(next(test))
+print(next(test))
+print(next(test))
+print(next(test))
+print(next(test))
+print(next(test))
+print(next(test))
+print(next(test))
