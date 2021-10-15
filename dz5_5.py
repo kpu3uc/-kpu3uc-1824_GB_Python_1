3e8e72c8a6909e5a38996ab7da89e146c3b6c90a
s4astie <serg_1982@mail.ru>
2021-10-15 21:08:39 +0300

    dz 5 5 fixes

--- dz5_5.py
+++ dz5_5.py
@@ -4,17 +4,19 @@
 # src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 # result = [23, 1, 3, 10, 4, 11]
 # Подсказка: напишите сначала решение «в лоб». Потом подумайте об оптимизации.
+from time import perf_counter
 
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+start = perf_counter()
 result = [_ for _ in src if src.count(_) == 1]
-print(result)
+print(result, perf_counter() - start)
 
+start = perf_counter()
 set_src = set()
-result1 = []
-
+result = []
 for _ in src:
     if _ not in set_src:
         set_src.add(_)
-        result1.append(_)
-print(result1)
-
+        if src.count(_) == 1:
+            result.append(_)
+print(result, perf_counter() - start)
