d04e4379dfbd971d646643f37b698d189a9ac601
s4astie <serg_1982@mail.ru>
2021-10-15 19:54:36 +0300

    dz 5 4 fixes

--- dz5_4.py
+++ dz5_4.py
@@ -5,13 +5,25 @@
 # ```
 #
 # Подсказка: использовать возможности python, изученные на уроке.
+from time import perf_counter
+from numpy import random
 
-src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+# src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
+src = [random.randint(100) for _ in range(1, 100000)]
 # print(src)
 
-result = [y for i, y in enumerate(src) if y > src[i-1]]
-print(result)
+start = perf_counter()
+result = [y for i, y in enumerate(src) if y > src[i-1]]  # неправильный вариант, но было весело
+# print(result, perf_counter() - start)
+print(perf_counter() - start)
 
-result = [y for i, y in zip(src, src[1:]) if y > i]
-print(result)
+start = perf_counter()
+result = [y for i, y in zip(src, src[1:]) if y > i]  # мой вариант быстрее
+# print(result, perf_counter() - start)
+print(perf_counter() - start)
+
+start = perf_counter()
+result = [src[i + 1] for i in range(len(src) - 1) if src[i + 1] > src[i]]  # подсмотрено у чатика
+# print(result, perf_counter() - start)
+print(perf_counter() - start)
