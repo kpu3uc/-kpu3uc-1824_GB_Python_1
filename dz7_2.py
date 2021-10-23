2aa77330c7051cb1dd2db103f9942d300b875117
s4astie <serg_1982@mail.ru>
2021-10-23 11:26:04 +0300

    dz 7 2

---
+++ dz7_2.py
@@ -0,0 +1,39 @@
+# 2. * (вместо 1) Написать скрипт, создающий из config.yaml стартер для проекта со следующей структурой:
+# |--my_project
+#    |--settings
+#    |  |--__init__.py
+#    |  |--dev.py
+#    |  |--prod.py
+#    |--mainapp
+#    |  |--__init__.py
+#    |  |--models.py
+#    |  |--views.py
+#    |  |--templates
+#    |     |--mainapp
+#    |        |--base.html
+#    |        |--index.html
+#    |--authapp
+#    |  |--__init__.py
+#    |  |--models.py
+#    |  |--views.py
+#    |  |--templates
+#    |     |--authapp
+#    |        |--base.html
+#    |        |--index.html
+# Примечание: структуру файла config.yaml придумайте сами, его можно создать в любом текстовом редакторе
+# «руками» (не программно); предусмотреть возможные исключительные ситуации, библиотеки использовать нельзя.
+
+# делаем тупо структуру c полными путями, первые уровни впереди,
+# вторичные во вторую очередь, третичные - в третью.
+import os
+
+with open("config.yaml", encoding="utf-8") as config:
+    while True:
+        line = config.readline().replace('\n', '')
+        if not line:
+            break
+        if not os.path.exists(line):
+            if "." in line:
+                open(line, 'a', encoding='utf-8').close()
+            else:
+                os.mkdir(line)
