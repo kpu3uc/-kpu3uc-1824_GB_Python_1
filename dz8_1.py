093806a4db1a665d6eda03e4abb21b9b86f08d7b
s4astie <serg_1982@mail.ru>
2021-10-26 18:44:50 +0300

    dz 8 1

---
+++ dz8_1.py
@@ -0,0 +1,26 @@
+# 1. Написать функцию email_parse(<email_address>), которая при помощи регулярного выражения извлекает
+# имя пользователя и почтовый домен из email адреса и возвращает их в виде словаря.
+# Если адрес не валиден, выбросить исключение ValueError. Пример:
+# >>> email_parse('someone@geekbrains.ru')
+# {'username': 'someone', 'domain': 'geekbrains.ru'}
+# >>> email_parse('someone@geekbrainsru')
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#   ...
+#     raise ValueError(msg)
+# ValueError: wrong email: someone@geekbrainsru
+# Примечание: подумайте о возможных ошибках в адресе и постарайтесь учесть их в регулярном выражении;
+# имеет ли смысл в данном случае использовать функцию re.compile()?
+import re
+
+
+def email_parse(email_address):
+    if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$', email_address):
+        msg = "wrong email: " + email_address
+        raise ValueError(msg)
+    username, domain = email_address.split("@")
+    return {"username": username, "domain": domain}
+
+
+print(email_parse("test@test.ru"))
+print(email_parse("test@test.r6"))
