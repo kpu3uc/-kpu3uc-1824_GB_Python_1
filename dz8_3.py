7738452b0fe65f0a91e3d36f8f331bb726f2eb88
s4astie <serg_1982@mail.ru>
2021-10-28 10:17:44 +0300

    dz 8 3

---
+++ dz8_3.py
@@ -0,0 +1,36 @@
+# 3. Написать декоратор для логирования типов позиционных аргументов функции, например:
+# def type_logger...
+#     ...
+#
+#
+# @type_logger
+# def calc_cube(x):
+#    return x ** 3
+#
+# >>> a = calc_cube(5)
+# 5: <class 'int'>
+# Примечание: если аргументов несколько - выводить данные о каждом через запятую;
+# можете ли вы вывести тип значения функции? Сможете ли решить задачу для именованных аргументов?
+# Сможете ли вы замаскировать работу декоратора? Сможете ли вывести имя функции, например, в виде:
+# >>> a = calc_cube(5)
+# calc_cube(5: <class 'int'>)
+
+def type_logger(func):
+    print("init")
+
+    def catcher(*args, **kwargs):
+        print("catcher")
+        with open("log.txt", "a", encoding="utf-8"):
+            ...
+
+        return print(type(*args, **kwargs))
+
+    return catcher
+
+
+@type_logger
+def calc_cube(x):
+    return x ** 3
+
+
+print(calc_cube(6))
