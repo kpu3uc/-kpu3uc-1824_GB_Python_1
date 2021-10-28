466ca2447c44b362fc48ff25df6dafd61166791a
s4astie <serg_1982@mail.ru>
2021-10-28 16:29:59 +0300

    dz 8 3 fix

--- dz8_3.py
+++ dz8_3.py
@@ -15,6 +15,7 @@
 # >>> a = calc_cube(5)
 # calc_cube(5: <class 'int'>)
 
+
 def type_logger(func):
     # print("init")
     # print(func)
@@ -22,21 +23,13 @@ def type_logger(func):
     def catcher(*args, **kwargs):
         # print("catcher")
         with open("logs.txt", "a", encoding="utf-8") as logs:  # замаскировали - в консоль не идёт, пишется в файл
-            logs.write(str(func))
-            logs.write("\n")
+            logs.write(f'{func.__name__}(')
             for arg in args:
-                logs.write(str(arg))
-                logs.write(": ")
-                logs.write(str(type(arg)))
-                logs.write("\n")  # получилось с новой строки, а не через запятую
+                logs.write(f'arg {str(arg)}: {str(type(arg))}, ')
             for arg in kwargs.values():
-                logs.write(str(arg))
-                logs.write(": ")
-                logs.write(str(type(arg)))
-                logs.write("\n")
-            logs.write("тип значения функции: ")
-            logs.write(str(type(func(*args, **kwargs))))  # тип значения функции
-            logs.write("\n")
+                logs.write(f'kwarg {str(arg)}: {str(type(arg))}, ')  # в конце остается лишняя запятая, но лень бороться
+            logs.write(")\n")
+            logs.write(f"тип значения функции: {str(type(func(*args, **kwargs)))}\n")
         return func(*args, **kwargs)
     return catcher
 
@@ -51,5 +44,5 @@ def multi_args(x, y, u=1):
     return x * y * u
 
 
-print(calc_cube(5))
-# print(multi_args(3, 4, u=10))
+# print(calc_cube(5))
+print(multi_args(3, 4, u=10))
