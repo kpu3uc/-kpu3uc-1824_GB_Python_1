6591f27b548574c6787439deb732681fe8185943
s4astie <serg_1982@mail.ru>
2021-10-28 15:57:22 +0300

    dz 8 3

--- dz8_3.py
+++ dz8_3.py
@@ -17,18 +17,27 @@
 
 def type_logger(func):
     # print("init")
+    # print(func)
 
     def catcher(*args, **kwargs):
         # print("catcher")
-        with open("log.txt", "a", encoding="utf-8") as logs:  # замаскировали - в консоль ничего не идёт, пишется в файл
-            logs.write(str(*args, **kwargs))
-            logs.write(": ")
-            logs.write(str(type(*args)))
-            logs.write(" , типа значения функции: ")
+        with open("logs.txt", "a", encoding="utf-8") as logs:  # замаскировали - в консоль не идёт, пишется в файл
+            logs.write(str(func))
+            logs.write("\n")
+            for arg in args:
+                logs.write(str(arg))
+                logs.write(": ")
+                logs.write(str(type(arg)))
+                logs.write("\n")  # получилось с новой строки, а не через запятую
+            for arg in kwargs.values():
+                logs.write(str(arg))
+                logs.write(": ")
+                logs.write(str(type(arg)))
+                logs.write("\n")
+            logs.write("тип значения функции: ")
             logs.write(str(type(func(*args, **kwargs))))  # тип значения функции
             logs.write("\n")
         return func(*args, **kwargs)
-
     return catcher
 
 
@@ -37,4 +46,10 @@ def calc_cube(x):
     return x ** 3
 
 
+@type_logger
+def multi_args(x, y, u=1):
+    return x * y * u
+
+
 print(calc_cube(5))
+# print(multi_args(3, 4, u=10))
