82d27371f1ca83e2a8355373c55bdd5d18ce998e
s4astie <serg_1982@mail.ru>
2021-10-30 14:10:02 +0300

    dz 9 2

--- dz9_2.py
+++ dz9_2.py
@@ -9,7 +9,13 @@
 # Например: 20 м*5000 м*25 кг*5 см = 12500 т.
 
 class Road:
-    """Дорога"""
+    """
+    Дорога
+    :param length - m
+    :param width - m
+    :param thickness - cm
+    :param mass - kg
+    """
     __length, __width, __thickness, __mass = int, int, int, int
 
     def __init__(self, length, width=3, thickness=1, mass=25):
@@ -22,11 +28,13 @@ class Road:
         return self.__length * self.__width * self.__mass * self.__thickness
 
 
-narrow_road = Road(50)
-print(narrow_road.lay_asphalt())
-ordinary_road = Road(50, 6)
-print(ordinary_road.lay_asphalt())
-thick_road = Road(50, 6, 10)
-print(thick_road.lay_asphalt())
-rough_road = Road(50, 6, 10, 50)
-print(rough_road.lay_asphalt())
+# narrow_road = Road(50)
+# print(narrow_road.lay_asphalt())
+# ordinary_road = Road(50, 6)
+# print(ordinary_road.lay_asphalt())
+# thick_road = Road(50, 6, 10)
+# print(thick_road.lay_asphalt())
+# rough_road = Road(50, 6, 10, 50)
+# print(rough_road.lay_asphalt())
+test_road = Road(5000, 20, 5, 25)  # длина, ширина, толщина, масса
+print(test_road.lay_asphalt())  # в кг
