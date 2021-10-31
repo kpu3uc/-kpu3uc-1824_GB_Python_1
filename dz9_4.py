d56ceb3d593d6d9439c9e14e87acda0fcb9bfae9
s4astie <serg_1982@mail.ru>
2021-10-31 10:00:42 +0200

    dz 9 4 fix

--- dz9_4.py
+++ dz9_4.py
@@ -8,7 +8,8 @@
 # При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
 
 class Car:
-    speed = 0
+    __speed = 0
+    direction = 0  # 0 - прямо, 1 - налево, 2 - направо
     possible_color = ("white", "black", "red", "green")
     color = possible_color[0]
     name = "Car"
@@ -21,14 +22,26 @@ class Car:
             raise ValueError("color not possible")
         self.name = name
 
-    def go(self):
-        pass
+    def go(self, speed: int = 10):
+        self.speed = speed
+        return f'{self.name} едет'
 
     def stop(self):
-        pass
+        self.speed = 0
+        return f'{self.name} стоит'
 
-    def turn(self, direction):
-        pass
+    def turn(self, direction: int = 0):
+        if direction == 0:
+            self.direction = direction
+            return f'{self.name} не поворачивает'
+        elif direction == 1:
+            self.direction = direction
+            return f"{self.name} поворачивает налево"
+        elif direction == 2:
+            self.direction = direction
+            return f"{self.name} поворачивает направо"
+        else:
+            raise ValueError("unexpected value")
 
     def show_speed(self):
         return self.speed
@@ -38,13 +51,11 @@ class TownCar(Car):
     possible_color = ("white", "black", "red", "green", "orange", "yellow", "blue", "purple", "pink", "gray")
     name = "TownCar"
 
-    def __init__(self, name: str, color_from_possible: int = 0):
-        super().__init__(name, color_from_possible)
-        if color_from_possible in range(len(self.possible_color)):
-            self.color = self.possible_color[color_from_possible]
+    def show_speed(self):
+        if self.speed <= 40:
+            return self.speed
         else:
-            raise ValueError("color not possible")
-        self.name = name
+            return f"{self.speed}, превышаем"
 
 
 class SportCar(Car):
@@ -59,7 +70,10 @@ class PoliceCar(Car):
     pass
 
 
+print()
 test_car = Car("Пусечка", 2)
-print(test_car.name, test_car.color, test_car.speed)
+print(test_car.name, test_car.color, test_car.show_speed())
+print(test_car.go(), test_car.turn(1))
 test_town_car = TownCar("Утипусечка", 6)
-print(test_town_car.name, test_town_car.color, test_town_car.speed)
+print(test_town_car.go(110), test_town_car.turn(1))
+print(test_town_car.name, test_town_car.color, test_town_car.show_speed())
