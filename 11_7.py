44c7d5303a4c9e4302ade66489852119f1c394f9
s4astie <serg_1982@mail.ru>
2021-11-10 20:37:31 +0200

    dz 11 7

---
+++ 11_7.py
@@ -0,0 +1,37 @@
+# 7. Реализовать проект «Операции с комплексными числами». Создайте класс «Комплексное число».
+# Реализуйте перегрузку методов сложения и умножения комплексных чисел. Проверьте работу проекта.
+# Для этого создаёте экземпляры класса (комплексные числа), выполните сложение и умножение созданных экземпляров.
+# Проверьте корректность полученного результата.
+from __future__ import annotations
+
+
+class Cotlex:
+
+    def __init__(self, cotlex: complex):
+        self.cotlex = cotlex
+
+    def __add__(self, other: Cotlex):
+        if isinstance(other, Cotlex):
+            return self.cotlex + other.cotlex
+
+    def __mul__(self, other: Cotlex):
+        if isinstance(other, Cotlex):
+            return self.cotlex * other.cotlex
+
+    def __str__(self):
+        return str(self.cotlex)
+
+    def __repr__(self):
+        return str(self.cotlex)
+
+
+print(3 + 7j)
+cotlex = Cotlex(3 + 7j)
+cotlex2 = Cotlex(4 + 2j)
+print(cotlex)
+print(type(cotlex))
+
+a = {cotlex}
+print(a)
+print(cotlex2 + cotlex)
+print(cotlex2 * cotlex)
